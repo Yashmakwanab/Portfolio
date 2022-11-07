713d451c3e8a458eff26f126ec21f5e20f9c44c7
YASH MAKWANA <72153542+Yashmakwanab@users.noreply.github.com>
2022-11-07 19:09:32 +0530

    app v1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and it Will test the desktop git
+          Edit <code>src/App.js</code> and it Will test the yash makwana
         </p>
         <a
           className="App-link"
